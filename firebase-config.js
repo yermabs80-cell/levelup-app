8253750b4ef6ca1bd07c15362dcab0db9625400e
unknown <Yermabs80@gmail.com>
2026-08-08 19:43:38 +0500

    Update project

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,10 @@
-/*
- * Вставьте сюда конфигурацию веб-приложения из Firebase Console.
- * Firebase Web config не является секретом, но реальные ключи и другие
- * учётные данные сервисных аккаунтов никогда не должны попадать в этот файл.
- */
-window.LEVELUP_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCdUy39dBBpM1sNQbkW-eAo1ySbigA7jAM",
+  authDomain: "poised-octane-473006-d8-c5830.firebaseapp.com",
+  projectId: "poised-octane-473006-d8-c5830",
+  storageBucket: "poised-octane-473006-d8-c5830.firebasestorage.app",
+  messagingSenderId: "842892348626",
+  appId: "1:842892348626:web:c4e928d397fd88aa0b310b",
+  measurementId: "G-Q27E0TX07J"
 };
